cc1f23d4716e92df8bb6ebedbc6a1d2524c248d4
natGalynker <natalgal@gmail.com>
2016-09-25 17:40:05 -0400

    Create sendActions
    add submit and cancel actions for the profile-update
    template
    in the profile-update template close the form tag

--- app/components/profile-update/component.js
+++ app/components/profile-update/component.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  tagName: 'div',
+  classNames: ['form-group'],
+
+  actions: {
+    cancel () {
+      let profile = this.get('profile');
+      this.sendAction('cancel', profile);
+    },
+
+    submit () {
+      let profile = this.get('profile');
+      this.sendAction('submit', profile);
+    },
+  }
 });
