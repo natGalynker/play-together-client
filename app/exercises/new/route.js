fb3a5e8ae3a7e323c22fbeb59b0cfcf280693569
natGalynker <natalgal@gmail.com>
2016-09-25 21:19:23 -0400

    Create route for new exercise
    created a transitionTo which will link to all exercises
    created a new sendAction for submit
    save the exercise to assigned to that user with the pertaining
    id

--- app/exercises/new/route.js
+++ app/exercises/new/route.js
@@ -1,4 +1,26 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-});
+  auth: Ember.inject.service(),
+  credentials: ('auth'),
+
+  model () {
+    let id = this.get('credentials.id');
+    return this.get('store').findRecord('profile', id);
+  },
+
+  actions: {
+    submit (data) {
+      let exercise = this.get('store').createRecord('exercise', data);
+      exercise.save()
+      .then((result) => {
+        console.log(result.id);
+      });
+      this.transitionTo('exercises');
+    },
+
+    cancel () {
+        this.transitionTo('exercises');
+      }
+    }
+  });
