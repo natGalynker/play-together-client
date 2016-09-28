10e4bf28e42269b0259adc6a36cd071189800d45
natGalynker <natalgal@gmail.com>
2016-09-28 09:45:12 -0400

    Change the environment for deploy
    change adapter.js and environment to route to the deployed

--- config/environment.js
+++ config/environment.js
@@ -6,7 +6,7 @@ module.exports = function(environment) {
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
-    // apiHost: 'http://localhost:3000',
+    apiHost: 'http://localhost:3000',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
@@ -41,6 +41,9 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
+    ENV.baseURL = '/';
+    ENV.locationType = 'hash';
+    ENV.apiHost = 'https://playful-log.herokuapp.com';
 
   }
 
